08a79792201443fafa6e37f9ae30889d68c3243c
CodeJuuun <Chomseng.a@gmail.com>
2025-02-13 15:16:28 -0500

    used jquery to wrap this keyword to allow use of .val and appended length to calculate num of chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,14 @@
 // ensures DOM has been loaded
 $(document).ready(function() {
-  console.log("Hello")
+  console.log("DOM is loaded")
+
+// declared variable via jquery syntax
+  const $textarea = $(".new-tweet textarea");
+
+  // logging "this" will display the entire DOM element
+  $textarea.on("input", function() { 
+    const lengthOfText = $(this).val().length
+    console.log("characters typed:", lengthOfText)
+  })
 });
+
