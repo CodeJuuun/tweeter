d91edbc04932204de394c76804d7ca5e0caadf4e
CodeJuuun <Chomseng.a@gmail.com>
2025-02-13 15:45:39 -0500

    added logic to find counter and based on current char length, add/remove class and change color to indicate it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,10 +5,24 @@ $(document).ready(function() {
 // declared variable via jquery syntax
   const $textarea = $(".new-tweet textarea");
 
-  // logging "this" will display the entire DOM element
+  // logging  just "this" will display the entire DOM element
   $textarea.on("input", function() { 
+    const maxLength = 140;
     const lengthOfText = $(this).val().length
-    console.log("characters typed:", lengthOfText)
+    const remainder = maxLength - lengthOfText
+    console.log("characters typed:", lengthOfText, remainder)
+
+    // traverse DOM tree to find node element that matches ".counter" relative to the textarea
+    const $counter = $(this).closest(".new-tweet").find(".counter");
+  
+    // update counter
+    $counter.text(remainder);
+  
+    if (remainder < 0) {
+      $counter.addClass("over-char-limit");
+    } else {
+      $counter.removeClass("over-char-limit");
+    }
   })
 });
 
